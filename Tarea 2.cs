6638a312ee60b8be8c0b437d7a94084250a248d4
Nicolás Bernal <53100121+SkarddY@users.noreply.github.com>
2019-07-19 20:31:46 -0500

    Add files via upload

---
+++ Tarea 2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problema 2
+            Console.Write("Para el triángulo rectangulo tzy ingrese los valores de t y el ángulo a ");
+            Console.WriteLine("");
+            Console.Write("Valor del lado t = ");
+            double t = double.Parse(Console.ReadLine());
+            Console.Write("Valor del ángulo a (en grados) = ");
+            double a = double.Parse(Console.ReadLine());
+
+            // Ángulo v
+            Console.Write("Valor del ángulo v = ");
+
+            double v = (180 - 90 - a);
+            Console.WriteLine(v);
+            double v1 = v * (Math.PI / 180);
+            
+            // Lado z
+            Console.Write("Valor del lado z = ");
+
+            double z = (Math.Sin(v1)*t / 1 );
+            Console.Write(z);
+
+            // Lado y
+            Console.Write(" Valor del lado y = ");
+
+            double y = Math.Sqrt(t * t - z * z);
+            Console.WriteLine(y);            
+
+
+
+        }
+    }
+}
---
+++ Tarea 3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Para el triángulo rectangulo JKZ ingrese los valores conocidos del lado Z y el ángulo n ");
+            Console.WriteLine(" ");
+            Console.Write("Valor lado Z = ");
+            double Z = double.Parse(Console.ReadLine());
+            Console.Write("Ángulo n (en grados) = ");
+            double n = double.Parse(Console.ReadLine());
+
+            //Ángulo m
+            Console.Write("Valor del ángulo m = ");
+            double m = (180 - 90 - n);
+            Console.WriteLine(m);
+
+            double n1 = (n * Math.PI) / 180.0;
+            double m1 = (m * Math.PI) / 180.0;
+
+            //Lado K
+            Console.Write("Valor del lado K = ");
+            double K = (Math.Sin(m1) * Z) / Math.Sin(n1);
+            Console.WriteLine(K);
+
+            //Lado J
+            Console.Write("Valor del lado J = ");
+            double J = Math.Sqrt(Z * Z + K * K);
+            Console.WriteLine(J);
+
+        }
+
+    }
+}
