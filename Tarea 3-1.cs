f1cf7c950f72eb075792524c24c6e33cd987547b
Nicolás Bernal <53100121+SkarddY@users.noreply.github.com>
2019-07-25 10:58:12 -0500

    Add files via upload

---
+++ Tarea 3-1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa tu salario mensual: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            double smmlv = 828116;
+
+            if (salario < smmlv * 2)
+            {
+                Console.Write("Pagas la tarifa A.");
+
+            }
+            else if (2 * smmlv <= salario && salario <= 4 * smmlv)
+            {
+                Console.Write("Pagas la tarifa B ");
+            }
+            else if (salario> 4 * smmlv){
+                Console.Write("Pagas la tarifa C ");
+            }
+
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
