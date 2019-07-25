ab3409ea0c5c499ed334cafbc90896a0b5b943b3
Nicolás Bernal <53100121+SkarddY@users.noreply.github.com>
2019-07-24 21:12:30 -0500

    Add files via upload

---
+++ Tarea 2/Tarea 2 - 3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Con los datos w, d y x, hallar y");
+
+            Console.Write("Lado w  ");
+            double w = double.Parse(Console.ReadLine());
+            Console.Write("Lado x ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Ángulo d ");
+            double dgrados = double.Parse(Console.ReadLine());
+
+            //Operaciones
+
+            double d = dgrados * Math.PI / 180.0;
+
+            double z = w * Math.Sin(d);
+
+            double f = Math.Sqrt(w * w - z * z);
+
+            double y = f - x;
+
+            Console.Write("Lado y ");
+            Console.WriteLine(y);
+        }
+
+
+    }
+}
