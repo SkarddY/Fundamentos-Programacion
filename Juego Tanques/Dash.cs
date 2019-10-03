1cb8a2f77151184a91c86527be7566f340c91b1a
Nicolás Bernal <53100121+SkarddY@users.noreply.github.com>
2019-10-02 22:22:07 -0500

    Update Dash.cs

--- Juego Tanques/Dash.cs
+++ Juego Tanques/Dash.cs
@@ -17,27 +17,31 @@ public class Motor : MonoBehaviour
 
     void Update()
     {
-        float magFuerza = 500;
+        float magFuerza = 300;
         float sentido = 1;
         Vector3 dir = transform.forward;
-        Vector3 fuerza = magFuerza * dir * sentido;
+        Vector3 fuerza1 = magFuerza * dir * sentido;
 
-        Vector3 direccion = transform.forward * Input.GetAxis("Vertical");        
+        Vector3 dir2 = new Vector3(0, 1, 0);
+        Vector3 fuerza2 = magFuerza * dir2 * sentido;
+
+        Vector3 direccion = transform.forward * Input.GetAxis("Vertical");
         Vector3 velocidad = magnitud * (direccion);
         Vector3 distancia = velocidad * Time.deltaTime;
-        transform.position += distancia;       
+        transform.position += distancia;
 
-        Vector3 direccionA = new Vector3(0, 1, 0) * Input.GetAxis("Horizontal");        
+        Vector3 direccionA = new Vector3(0, 1, 0) * Input.GetAxis("Horizontal");
         Vector3 velAng = magnitudAng * direccionA;
         Vector3 distAng = velAng * Time.deltaTime;
         transform.eulerAngles += distAng;
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rbd.AddForce(fuerza);
+            rbd.AddForce(fuerza1);
+            rbd.AddForce(fuerza2);
         }
 
-        if (velocidad.magnitude>1||velAng.magnitude>1)
+        if (velocidad.magnitude > 1 || velAng.magnitude > 1)
         {
             audioMotor.clip = driving;
             if (!audioMotor.isPlaying) audioMotor.Play();
